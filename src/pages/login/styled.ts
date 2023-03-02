1603fc39e0a7bc5763dafee9c82cec7ca17694ba
zahideaksak <zahide.aksaak@gmail.com>
2023-03-02 09:57:03 +0300

    src added

--- src/pages/login/styled.ts
+++ src/pages/login/styled.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 20,
+        backgroundColor: "white",
     },
     topView: {
         paddingTop: 50,
